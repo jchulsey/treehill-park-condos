cdae49143a8da8a6008e02d034d772d319ad22cf
J <j.conrad.hulsey@gmail.com>
2024-01-10 22:17:57 -0600

    CCR updates

--- src/pages/CCR.js
+++ src/pages/CCR.js
@@ -3,22 +3,62 @@
 export default function CCR() {
     return (
         <div className="CCR">
-            <h2>TL;DR</h2>
+            <article>
+                <h1>TL;DR</h1>
+                    <p>
+                    This document outlines the rules and regulations for the Treehill Park Property Owners Association Inc. 
+                    The key points include definitions of terms, property rights, membership and voting rights, covenants for 
+                    maintenance and assessments, architectural control, general provisions, exterior maintenance, use restrictions, 
+                    party walls, and insurance. It specifies the association's authority over common areas, voting rights, assessments 
+                    for property maintenance, architectural approval, and insurance coverage. The document also covers provisions for
+                    amendments, annexation, and the duration of covenants.
+                    </p>
+            </article>
             <article>
                 <h1>Declaration of Covenants, Conditions, and Restrictions (CC&R)</h1>
-
+                <h2>Article I: Definitions</h2>
+                
                 <section>
-                    <h2>Article I: Definitions</h2>
                     <p><strong>SECTION 1. </strong> 
-                        <em>Association</em> shall mean...
+                        <em>Association</em> shall mean and refer to the Treehill Park Property Owners Association Inc., its successors, 
+                        and assigns. 
                     </p>
-                    
                 </section>
 
                 <section>
-                    <h2>Article II: Property Rights</h2>
-                    <p><strong>SECTION 1:</strong> Owners Easements of Enjoyment: Every owner shall have a right...</p>                 
+                    <p><strong>SECTION 2. </strong> 
+                        <em>Owner</em> shall mean and refer to the record owner, whether one or more persons or entities, of a free 
+                        simple title to any Lot is a part of the Properties, including contract sellers, but excluding those having such 
+                        interest merely as security for the performance of an obligation.
+                    </p>  
                 </section>
+
+                <section>
+                    <p><strong>SECTION 3. </strong> 
+                        <em>Properties</em> shall mean and refer to that certain real property hereinbefore described, and such additions 
+                        thereto as my hereafter be brought within the jurisdiction of the Association. 
+                    </p>  
+                </section>
+
+                <section>
+                    <p><strong>SECTION 4. </strong> 
+                        <em>Common Area</em> shall mean all real property including the improvements thereto owned by the Association for 
+                        the common use and enjoyment of the owners. The Common Area to be owned by the Association at the time of the 
+                        conveyance of the first lot is described as follows:
+                        <br />
+                        <strong>Tract A</strong>, Treehill Park, a planned unit development, city of Sherwood, Pulaski County, Arkansas, as shown on plat 
+                        filed in plat book B, page 244 with the Circuit Clerk and Recorder of Pulaski County, Arkansas on June 14, 1984.
+                    </p>  
+                </section>
+
+            </article>
+            <article>
+
+            <section>
+                <h2>Article II: Property Rights</h2>
+                    <p><strong>SECTION 1:</strong> Owners Easements of Enjoyment: Every owner shall have a right...</p>                 
+            </section>
+
             </article>
         </div>
     );
