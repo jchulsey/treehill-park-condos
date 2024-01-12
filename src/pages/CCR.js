4b6c864dfe1a9b0f1e889f44d55874385c17327c
J <j.conrad.hulsey@gmail.com>
2024-01-11 18:49:45 -0600

    Started creating cards and updated meta data

--- src/pages/CCR.js
+++ src/pages/CCR.js
@@ -1,19 +1,9 @@
-
+import TLDR from './components/TLDRCard';
 
 export default function CCR() {
     return (
-        <div className="CCR">
-            <article>
-                <h1>TL;DR</h1>
-                    <p>
-                    This document outlines the rules and regulations for the Treehill Park Property Owners Association Inc. 
-                    The key points include definitions of terms, property rights, membership and voting rights, covenants for 
-                    maintenance and assessments, architectural control, general provisions, exterior maintenance, use restrictions, 
-                    party walls, and insurance. It specifies the association's authority over common areas, voting rights, assessments 
-                    for property maintenance, architectural approval, and insurance coverage. The document also covers provisions for
-                    amendments, annexation, and the duration of covenants.
-                    </p>
-            </article>
+        <div className="ccr">
+            <TLDR />
             <article>
                 <h1>Declaration of Covenants, Conditions, and Restrictions (CC&R)</h1>
                 <h2>Article I: Definitions</h2>
---
+++ src/pages/components/TLDRCard.js
@@ -0,0 +1,17 @@
+export default function TLDR() {
+    return (
+        <div className="cards">
+            <article className="card">
+                <h1>TL;DR</h1>
+                    <p>
+                    This document outlines the rules and regulations for the Treehill Park Property Owners Association Inc. 
+                    The key points include definitions of terms, property rights, membership and voting rights, covenants for 
+                    maintenance and assessments, architectural control, general provisions, exterior maintenance, use restrictions, 
+                    party walls, and insurance. It specifies the association's authority over common areas, voting rights, assessments 
+                    for property maintenance, architectural approval, and insurance coverage. The document also covers provisions for
+                    amendments, annexation, and the duration of covenants.
+                    </p>
+            </article>
+        </div>
+    )
+}
